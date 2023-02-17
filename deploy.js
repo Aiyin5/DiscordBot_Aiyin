f3e29970312347ea7f0fc840d392ce66b20eaa37
aiyin <372020407@qq.com>
2023-02-17 19:33:16 +0800

    update project

--- deploy.js
+++ deploy.js
@@ -1,4 +1,5 @@
-const { REST, Routes } = require('discord.js');
+const { Routes } = require('discord.js');
+const { REST } = require("@discordjs/rest");
 const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 
@@ -13,7 +14,7 @@ for (const file of commandFiles) {
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '9' }).setToken(token);
 
 // and deploy your commands!
 (async () => {
