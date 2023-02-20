8d937c966e664138ca3ee84cd8997d33637221d9
aiyin <372020407@qq.com>
2023-02-20 15:36:51 +0800

    update model for chatgpt

--- src/functions/handelMessage.js
+++ src/functions/handelMessage.js
@@ -1,23 +1,10 @@
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events } = require('discord.js');
 module.exports = (client) => {
     client.handleMessage = async () => {
-        client.on(Events.InteractionCreate, async interaction => {
+        client.on('interactionCreate', async interaction => {
             console.log(interaction);
             if(interaction.content=='p'){
                 await interaction.reply({content:'q'});
-            }
-            if (!interaction.isChatInputCommand()) return;
-
-            if (interaction.commandName === 'button') {
-                const row = new ActionRowBuilder()
-                    .addComponents(
-                        new ButtonBuilder()
-                            .setCustomId('primary')
-                            .setLabel('Click me!')
-                            .setStyle(ButtonStyle.Primary),
-                    );
-
-                await interaction.reply({ content: 'I think you should,', components: [row] });
             }
         });
     }
