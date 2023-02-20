fcb9ac4c4ca7cb22d1a2aa9bd0591eaeabfc01af
aiyin <372020407@qq.com>
2023-02-20 15:26:22 +0800

    update model for chatgpt

--- src/functions/handelMessage.js
+++ src/functions/handelMessage.js
@@ -2,6 +2,10 @@ const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events } = require('discor
 module.exports = (client) => {
     client.handleMessage = async () => {
         client.on(Events.InteractionCreate, async interaction => {
+            console.log(interaction);
+            if(interaction.content=='p'){
+                await interaction.reply({content:'q'});
+            }
             if (!interaction.isChatInputCommand()) return;
 
             if (interaction.commandName === 'button') {
