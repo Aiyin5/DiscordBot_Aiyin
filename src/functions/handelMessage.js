1945dbe69d3cc69af7e7803cb8d28547759b72f2
aiyin <372020407@qq.com>
2023-02-20 15:04:00 +0800

    update model for chatgpt

--- src/functions/handelMessage.js
+++ src/functions/handelMessage.js
@@ -1,9 +1,11 @@
 module.exports = (client) => {
     client.handleMessage = async () => {
         client.on('messageCreate', message => {
-            if (message.content === 'ping')
-            { message.channel.send('Pong!');
-            };
-        }
+                if (message.content === 'ping') {
+                    message.channel.send('Pong!');
+                }
+                ;
+            }
         )
+    }
 }
