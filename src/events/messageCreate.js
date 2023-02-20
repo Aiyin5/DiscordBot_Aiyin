33b88e7e63b2ae92dbe5be945dd68fd8ef94fc2d
aiyin <372020407@qq.com>
2023-02-20 19:53:39 +0800

    update model for chatgpt

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,8 +1,16 @@
 const { Interaction } = require("discord.js");
+const{data}=require('../../beforInterception.json');
+const map = new Map(Object.entries(data));
 
 module.exports = {
     name: 'messageCreate',
     async execute(message, client) {
-        console.log(message);
+        console.log(message.content);
+        map.forEach( async (value, key) => {
+            if(message.content.indexOf(key)!=-1){
+                console.log(value);
+                await message.reply({content:value});
+            }
+        });
     },
 };
