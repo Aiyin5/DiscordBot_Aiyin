bf677444a5e94331367adfe282eea6804fb46fbb
aiyin <372020407@qq.com>
2023-02-23 23:47:39 +0800

    update for interception

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -36,6 +36,8 @@ module.exports = {
         if(!isQuestion(content)){
             return;
         }
+        console.log(content);
+        console.log(client.nounwordsmap.keys());
         if(content.includes("什么")||content.includes("链接")){
             for (let i in client.nounwordsmap.keys()) {
                 if(content.includes(i)){
