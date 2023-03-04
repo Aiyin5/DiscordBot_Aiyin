1db63a8ba1b8748a652579af76c17e1a6de62570
aiyin <372020407@qq.com>
2023-03-04 15:32:57 +0800

    update for servr file Change

--- src/commands/normal/ping.js
+++ src/commands/normal/ping.js
@@ -3,10 +3,10 @@ const instacnce = require('../../util/caInstance')
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('getCurrentArr')
+		.setName('getping')
 		.setDescription('获取当前的前置拦截!'),
 	async execute(interaction) {
 		let arr=instacnce.getArray();
-		return interaction.reply(arr.toString());
+		return interaction.reply(arr);
 	},
 };
