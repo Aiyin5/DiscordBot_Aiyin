bc93e7c5b5260c237d0dafd7034e3be76b9e53af
aiyin <372020407@qq.com>
2023-03-08 00:21:09 +0800

    update for servr file Change

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'guildMemberAdd',
     async execute(guildmember, client,chatBot) {
         console.log(guildmember);
-        let user=guildmember.user;
-        await guildmember.send(`hello,\`${user}\``)
+        let user=guildmember.user.username;
+        //await guildmember.send(`hello,\`${user}\``)
     },
 };
