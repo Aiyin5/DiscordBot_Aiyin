03fe1d2f748f6a262820ea59d86cf917da02cbc1
aiyin <372020407@qq.com>
2023-03-07 17:34:15 +0800

    update for servr file Change

--- src/commands/normal/avatar.js
+++ src/commands/normal/avatar.js
@@ -1,13 +1,23 @@
 const { SlashCommandBuilder } = require('discord.js');
-
+const openai = require('../../util/chatBot')
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('获取头像')
 		.setDescription('Get the avatar URL of the selected user, or your own avatar.')
-		.addUserOption(option => option.setName('target').setDescription('The user\'s avatar to show')),
+		.addStringOption(option => option.setName('Description').setDescription('描述一下你希望的头像样子')),
 	async execute(interaction) {
-		const user = interaction.options.getUser('target');
-		if (user) return interaction.reply(`${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`);
-		return interaction.reply(`Your avatar: ${interaction.user.displayAvatarURL()}`);
+		const str = interaction.options.getString('Description');
+		let prt="create an avatar which is "+str;
+		try {
+			const response = await openai.createImage({
+				prompt: prt,
+				n: 1,
+				size: "256x256",
+			});
+			return interaction.reply(response.data[0].url);
+		}
+		catch (err){
+			return interaction.reply(err);
+		}
 	},
 };
